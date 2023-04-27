12d37d4b164a51d1c33048602ae0119d59065d5d
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-04-27 14:57:52 +0700

    inven done, td:show inven, combining stuff

---
+++ Assets/Scripts/Game/Inventory/Inventory.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static event Action<List<InventoryItem>> OnInventoryChange;
+
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+    private Dictionary<ItemData, InventoryItem> itemDictionary = new Dictionary<ItemData, InventoryItem>();
+
+    private void OnEnable()
+    {
+        //subscribing to onstuff collected
+        PVP.OnPVPCollected += Add;
+        I2.OnI2Collected+= Add;
+        Element1.OnElement1Collected += Add;
+        Element2.OnElement2Collected += Add;
+    }
+
+    private void OnDisable()
+    {
+        //unsubscribing to onstuff collected
+        PVP.OnPVPCollected -= Add;
+        I2.OnI2Collected -= Add;
+        Element1.OnElement1Collected -= Add;
+        Element2.OnElement2Collected -= Add;
+    }
+
+    public void Add(ItemData itemData)
+    {
+        if(itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.AddToStack();
+            Debug.Log($"{item.itemData.displayName} total stack is now {item.stackSize}");
+
+            OnInventoryChange?.Invoke(inventory);
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(itemData);
+            inventory.Add(newItem);
+            itemDictionary.Add(itemData, newItem);
+            Debug.Log($"Added {itemData.displayName} to the inventory for the first time");
+
+            OnInventoryChange?.Invoke(inventory);
+        }
+    }
+
+    public void Remove(ItemData itemData)
+    {
+        if (itemDictionary.TryGetValue(itemData, out InventoryItem item))
+        {
+            item.RemoveFromStack();
+            if(item.stackSize == 0)
+            {
+                inventory.Remove(item);
+                itemDictionary.Remove(itemData);
+            }
+
+            OnInventoryChange?.Invoke(inventory);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Inventory/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    public string displayName;
+    public string description;
+    public Sprite icon;
+}
